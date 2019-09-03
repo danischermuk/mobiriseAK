afad763c0b79c1d1e7dbd3ac26e60bfc81746b5b
danischermuk <daniel.schermuk@gmail.com>
2019-09-03 16:17:37 -0300

    Se mejora la subida de imagenes para que no sean pesadas

--- admin/api/upload.php
+++ admin/api/upload.php
@@ -1,54 +1,112 @@
-<?php 
+<?php
 
 require_once('../Autho.php');
 $token = null;
-        $token = "";
-        if (isset($_POST['token'])) {$token = $_POST['token'];}
-        if (isset($_GET['token'])) {$token = $_GET['token'];}
-        
-        
-        if (Autho::verifyJWT($token)) {
-            $requestMethod = $_SERVER['REQUEST_METHOD'];
-            // retrieve the inbound parameters based on request type.
-            switch($requestMethod) {
-                
+$token = "";
+if (isset($_POST['token'])) {
+    $token = $_POST['token'];
+}
+if (isset($_GET['token'])) {
+    $token = $_GET['token'];
+}
 
-                case 'POST':
 
-                if(!empty($_FILES['image'])){
-                    $ext = pathinfo($_FILES['image']['name'],PATHINFO_EXTENSION);
-                            $image = time().'.'.$ext;
-                            move_uploaded_file($_FILES["image"]["tmp_name"], '../../images/'.$image);
-                    echo $image;
-                }else{
-                    echo "";
+if (Autho::verifyJWT($token)) {
+    $requestMethod = $_SERVER['REQUEST_METHOD'];
+    // retrieve the inbound parameters based on request type.
+    switch ($requestMethod) {
+
+
+        case 'POST':
+
+            if (!empty($_FILES['image'])) {
+
+
+                $file = $_FILES['image']['tmp_name'];
+                $sourceProperties = getimagesize($file);
+                $fileNewName = time();
+                $folderPath = "../../images/";
+                $ext = pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
+                $imageType = $sourceProperties[2];
+
+                switch ($imageType) {
+
+
+                    case IMAGETYPE_PNG:
+                        $imageResourceId = imagecreatefrompng($file);
+                        $targetLayer = imageResize($sourceProperties, $imageResourceId, $sourceProperties[0], $sourceProperties[1]);
+                        imagepng($targetLayer, $folderPath . $fileNewName . "_thum." . $ext);
+                        break;
+
+                    case IMAGETYPE_JPEG:
+                        $imageResourceId = imagecreatefromjpeg($file);
+                        $targetLayer = imageResize($sourceProperties, $imageResourceId, $sourceProperties[0], $sourceProperties[1]);
+                        imagejpeg($targetLayer, $folderPath . $fileNewName . "_thum." . $ext);
+                        break;
+
+
+                    default:
+                        echo "Invalid Image type.";
+                        exit;
+                        break;
                 }
 
-
-                if(!empty($_FILES['file'])){
-                    $ext = pathinfo($_FILES['file']['name'],PATHINFO_EXTENSION);
-                            $cert = time().'.'.$ext;
-                            move_uploaded_file($_FILES["file"]["tmp_name"], '../../certificados/'.$cert);
-                    echo $cert;
-                }else{
-                    echo "";
-                }
-
-
-
-
-                break;
-                
-                
-
-
-            default:
-                $returnArray = array('error' => 'You have requested an invalid method.');
-                $jsonEncodedReturnArray = json_encode($returnArray, JSON_PRETTY_PRINT);
-                echo $jsonEncodedReturnArray;
+                $ext = pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
+                $image = time() . '.' . $ext;
+                move_uploaded_file($_FILES["image"]["tmp_name"], '../../images/' . $image);
+                move_uploaded_file($file, $folderPath. $fileNewName. ".". $ext);
+                echo $fileNewName . "_thum." . $ext;
+            } else {
+                echo "";
             }
-        } 
-        else {
-            $returnArray = array('error' => 'You are not logged in with a valid token. UPLOAD');
-            echo  json_encode($returnArray, JSON_PRETTY_PRINT);
-        }
+
+
+            if (!empty($_FILES['file'])) {
+                $ext = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
+                $cert = time() . '.' . $ext;
+                move_uploaded_file($_FILES["file"]["tmp_name"], '../../certificados/' . $cert);
+                echo $cert;
+            } else {
+                echo "";
+            }
+
+
+
+
+            break;
+
+
+
+
+        default:
+            $returnArray = array('error' => 'You have requested an invalid method.');
+            $jsonEncodedReturnArray = json_encode($returnArray, JSON_PRETTY_PRINT);
+            echo $jsonEncodedReturnArray;
+    }
+} else {
+    $returnArray = array('error' => 'You are not logged in with a valid token. UPLOAD');
+    echo  json_encode($returnArray, JSON_PRETTY_PRINT);
+}
+
+function imageResize($sourceProperties, $imageResourceId, $width, $height)
+{
+
+    if ($sourceProperties[0] > $sourceProperties[1]) {
+        //wide image
+        $targetWidth = 200;
+        $targetHeight = 200*$sourceProperties[1]/$sourceProperties[0];
+
+    } else {
+        $targetHeight = 200;
+        $targetWidth = 200*$sourceProperties[0]/$sourceProperties[1];
+    }
+
+    
+
+
+    $targetLayer = imagecreatetruecolor($targetWidth, $targetHeight);
+    imagecopyresampled($targetLayer, $imageResourceId, 0, 0, 0, 0, $targetWidth, $targetHeight, $width, $height);
+
+
+    return $targetLayer;
+}
